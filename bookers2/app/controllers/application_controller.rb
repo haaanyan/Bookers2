e32972f368ae1ea931896250515af2bc1b162714
haaanyan <kiha.0327@icloud.com>
2021-04-05 12:06:33 +0000

    [Fix2]4-10

--- bookers2/app/controllers/application_controller.rb
+++ bookers2/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image_id, :introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image, :introduction])
   end
 end
--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = Refile.secret_key = '63c4ff3fc25a5f55c2bca637e09f41a81ad5b5ee5ee8484ab5e25acf277a6d1d32e18d2a97ae0455a8b4d059ca9c9acd855005fe3480be4449ce9d99d5f047ac'
