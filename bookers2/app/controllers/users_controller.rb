43ac2c4b06a04bb20a0e10466a9d754e4dad0039
haaanyan <kiha.0327@icloud.com>
2021-04-08 14:30:55 +0000

    [Fix5]4-10

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -4,20 +4,30 @@ class UsersController < ApplicationController
 
 def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.where(user_id: @user.id)
 end
 
 def index
-    @user = User.find(params[:id])
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
 end
 
 def edit
     @user = User.find(params[:id])
+    if @user == current_user
+        render "edit"
+    else
+        redirect_to user_path(current_user)
+    end
 end
 
 def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    redirect_to user_path(current_user)
 end
 
 private
