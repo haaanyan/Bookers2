ba517db2b00f4ce3cd1b98d686e0a13b027e8cb6
haaanyan <kiha.0327@icloud.com>
2021-05-02 12:00:49 +0000

    コメント機能追加1

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ def show
     @book_new = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
 end
 
 def index
--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites
+  has_many :book_comments, dependent: :destroy
 
   attachment :profile_image
 
