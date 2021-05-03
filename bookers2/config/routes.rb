fb50521f3e87182c02b56534de091880af55d22b
haaanyan <kiha.0327@icloud.com>
2021-05-03 07:01:21 +0000

    フォロー・フォロワー機能追加

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
 
   get "/home/about" => "homes#about"
 
-  resources :users
+  resources :users do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+end
 
   resources :books do
-   resource :favorites, only: [:create, :destroy]
-   resources :book_comments, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
 
   end
 
