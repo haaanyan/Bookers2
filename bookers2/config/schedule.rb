11692134cdc2e11b799eac19459460440759fd03
haaanyan <kiha.0327@icloud.com>
2021-05-10 04:04:34 +0000

    Fixバッチ処理

--- bookers2/config/schedule.rb
+++ bookers2/config/schedule.rb
@@ -23,6 +23,6 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = Rails.env.to_sym
 set :environment, rails_env
 set :output, 'log/cron.log'
-every 1.minute do
+every 1.day, at: '10:00 am' do
   runner "Batch::Daily.new.mail"
   end
